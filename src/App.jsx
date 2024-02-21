a4677daa69da7ed33fb6072db6fd02ba7767f0e6
Pia Lopez <ppialopez@gmail.com>
2024-02-21 14:37:07 -0300

    agregue portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import About from './components/about/About'
 import Home from './components/home/Home'
+import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Services/>
     </main> 
     </>
   )
